27a7fe670a2a7694592bb3330d90c0525798ce72
Rajarshi Roy <royyraa@outlook.com>
2025-09-21 23:30:06 -0400

    Update Astro config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import { template } from "./src/settings";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+// Astro config for GitHub Pages user site
 export default defineConfig({
     integrations: [react(), tailwind(), sitemap()],
-    site: template.website_url,
-    base: template.base,
+    site: 'https://royyraa.github.io',
+    base: '/',
 });
+
